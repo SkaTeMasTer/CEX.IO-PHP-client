af67094ba8a355ada148b7920bdb364802d14cbc
MasterX1582 <davy1582@msn.com>
2013-11-03 06:05:50 +0100

    Update config.inc.sample.php

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -8,7 +8,7 @@
  * then adjust according to your profile at https://cex.io.
  * 
  * This sample is made available by MasterX1582.
- * If this is usefull for you, feel free to support me by sending some coins to 1MegaXG1bd6mTEQCdAMjVzGexcYrF5LJKv
+ * If this is usefull for you, feel free to support me by sending some bitcoins to 1MegaXG1bd6mTEQCdAMjVzGexcYrF5LJKv
  * I can absolutely not be held responsible by any losses made by use of this piece of code. 
  * You are the only person totaly responsible for running this software.
  * 
