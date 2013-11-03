4f3d9e5501132271201cb756e009f4abd9539d93
MasterX1582 <davy1582@msn.com>
2013-11-03 05:57:06 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  * CEX.IO - Bitcoin 
  *
  * This sample is made available by MasterX1582.
- * If this is usefull for you, feel free to support me by sending some coint to 1MegaXG1bd6mTEQCdAMjVzGexcYrF5LJKv
+ * If this is usefull for you, feel free to support me by sending some coins to 1MegaXG1bd6mTEQCdAMjVzGexcYrF5LJKv
  * I can absolutely not be held responsible by any losses made by use of this code. 
  * You are the only person totaly responsible for running this software.
  * 
