3d569183baf086865f47c05e43e8bf0f0331bbfc
MasterX1582 <davy1582@msn.com>
2013-11-03 05:56:25 +0100

    Update config.inc.sample.php

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -1,5 +1,7 @@
 <?php
 /*
+ * CEX.IO - Bitcoin 
+ *
  *              *** API settings ***
  *
  * Copy config.inc.sample.php to config.inc.php,
@@ -9,6 +11,8 @@
  * If this is usefull for you, feel free to support me by sending some coint to 1MegaXG1bd6mTEQCdAMjVzGexcYrF5LJKv
  * I can absolutely not be held responsible by any losses made by use of this code. 
  * You are the only person totaly responsible for running this software.
+ * 
+ * Enjoy!
  */
 define('USERNAME','<YOURUSERNAME>');
 define('KEY','<YOURAPIKEY>');
