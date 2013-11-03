ddb7d2eb1cd318bab2d2f1cedcf51301e4abb9f1
MasterX1582 <davy1582@msn.com>
2013-11-03 04:44:59 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,34 +4,39 @@ include('cexio-grabber.php');
 
 $balance = json_encode(cexio_query('https://cex.io/api/balance/'));
 $balance_o = json_decode($balance,true);
-
-if ($balance_o['BTC']['available']> 0){
-	echo "Bitcoins Available: <br>";
-	print $balance_o['BTC']['available'];
-	echo " &#3647;";
+if (isset($balance_o['BTC']['available'])){
+	if ($balance_o['BTC']['available']> 0){
+		echo "Bitcoins Available: <br>";
+		print $balance_o['BTC']['available'];
+		echo " &#3647;";
+	}
+}
+if (isset($balance_o['BTC']['orders'])){
+	if ($balance_o['BTC']['orders']> 0){
+		echo "<br>";
+		echo "<br>";
+		echo "GigaHash for Sale: <br>";
+		print $balance_o['BTC']['orders'];
+		echo " &#3647;";
+	}
+}
+if (isset($balance_o['GHS']['available'])){
+	if ($balance_o['GHS']['available']> 0){
+		echo "<br>";
+		echo "<br>";
+		echo "Gigahash Available: <br>";
+		print $balance_o['GHS']['available'];
+		echo " GHS";
+	}
 }
 
-if ($balance_o['BTC']['orders']> 0){
-	echo "<br>";
-	echo "<br>";
-	echo "GigaHash for Sale: <br>";
-	print $balance_o['BTC']['orders'];
-	echo " &#3647;";
-}
-
-if ($balance_o['GHS']['available']> 0){
-	echo "<br>";
-	echo "<br>";
-	echo "Gigahash Available: <br>";
-	print $balance_o['GHS']['available'];
-	echo " GHS";
-}
-
-if ($balance_o['GHS']['orders']> 0){
-	echo "<br>";
-	echo "<br>";
-	echo "Gigahash Ordered: <br>";
-	print $balance_o['GHS']['orders'];
-	echo " GHS";
+if (isset($balance_o['GHS']['orders'])){
+	if ($balance_o['GHS']['orders']> 0){
+		echo "<br>";
+		echo "<br>";
+		echo "Gigahash Ordered: <br>";
+		print $balance_o['GHS']['orders'];
+		echo " GHS";
+	}
 }
 ?>
