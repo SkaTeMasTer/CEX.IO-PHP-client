9f2c188dec933a65053667ea8476c7b89ee8283e
MasterX1582 <davy1582@msn.com>
2013-11-03 04:01:59 +0100

    Create config.inc.sample.php

---
+++ config.inc.sample.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ *              *** API settings ***
+ *
+ * Copy config.inc.sample.php to config.inc.php,
+ * then adjust according to your profile at https://cex.io.
+ */
+define('USERNAME','<YOURUSERNAME>');
+define('KEY','<YOURAPIKEY>');
+define('SECRET','<YOURAPISECRET>');
+?>
