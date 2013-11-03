b3c88d5cc88eece52e568454d7790598bd16201a
MasterX1582 <davy1582@msn.com>
2013-11-03 05:55:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,14 @@
 <?php 
+/*
+ * CEX.IO - Bitcoin 
+ *
+ * This sample is made available by MasterX1582.
+ * If this is usefull for you, feel free to support me by sending some coint to 1MegaXG1bd6mTEQCdAMjVzGexcYrF5LJKv
+ * I can absolutely not be held responsible by any losses made by use of this code. 
+ * You are the only person totaly responsible for running this software.
+ *
+ */
+ 
 include('config.inc.php');
 include('cexio-grabber.php'); 
 
