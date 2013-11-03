49059a5204ef8b04d6817277669bec4be198cf00
MasterX1582 <davy1582@msn.com>
2013-11-03 05:53:53 +0100

    Update config.inc.sample.php

--- config.inc.sample.php
+++ config.inc.sample.php
@@ -4,6 +4,11 @@
  *
  * Copy config.inc.sample.php to config.inc.php,
  * then adjust according to your profile at https://cex.io.
+ * 
+ * This sample is made available by MasterX1582.
+ * If this is usefull for you, feel free to support me by sending some coint to 1MegaXG1bd6mTEQCdAMjVzGexcYrF5LJKv
+ * I can absolutely not be held responsible by any losses made by use of this code. 
+ * You are the only person totaly responsible for running this software.
  */
 define('USERNAME','<YOURUSERNAME>');
 define('KEY','<YOURAPIKEY>');
