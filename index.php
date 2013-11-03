40ed1a0b711c0f17783a9c640436c93876693769
MasterX1582 <davy1582@msn.com>
2013-11-03 05:55:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,8 @@
  * If this is usefull for you, feel free to support me by sending some coint to 1MegaXG1bd6mTEQCdAMjVzGexcYrF5LJKv
  * I can absolutely not be held responsible by any losses made by use of this code. 
  * You are the only person totaly responsible for running this software.
- *
+ * 
+ * Enjoy!
  */
  
 include('config.inc.php');
